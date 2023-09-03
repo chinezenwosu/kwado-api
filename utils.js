2fa26f7d63820a42705ac78809c76dbf4526a8be
Chineze Nwosu <nwosuchineze@gmail.com>
2023-09-04 00:32:52 +0100

    create method for cleaning model properties

--- utils.js
+++ utils.js
@@ -4,4 +4,16 @@ const getDocumentHash = (key) => {
   return `${DOCUMENT_PREFIX}${key}`
 }
 
-export { DOCUMENT_PREFIX, getDocumentHash }
+const stripModel = (model, includeTimestamps = false) => {
+  if (model.toObject) {
+    const { _id, __v, createdAt, updatedAt, ...otherFields } = model.toObject()
+
+    if (includeTimestamps) {
+      return { createdAt, updatedAt, ...otherFields }
+    }
+    return { ...otherFields }
+  }
+  return model
+}
+
+export { DOCUMENT_PREFIX, getDocumentHash, stripModel }
