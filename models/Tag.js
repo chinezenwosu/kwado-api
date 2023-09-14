338bf6a418362a386a1f1277cded36df173bf951
Chineze Nwosu <nwosuchineze@gmail.com>
2023-09-14 02:06:58 +0100

    add tag schema

---
+++ models/Tag.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose')
+
+const tagSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Tag name is required'],
+    unique: [true, 'Tag name has to be unique'],
+  },
+  description: {
+    type: String
+  },
+  slug: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+})
+
+const Tag = model('Tag', tagSchema)
+module.exports = Tag
